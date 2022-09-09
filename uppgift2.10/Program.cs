a8b15c148f9d6274f431007bc00df5e3812c7208
E-Svensson <111962443+E-Svensson@users.noreply.github.com>
2022-09-09 11:08:09 +0200

    Add project files.

---
+++ uppgift2.10/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace Uppgift_2._1
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("vilken är din fovarit bok");
+            string Jagärzlatan = Console.ReadLine();
+            Console.WriteLine("roligt bok att läsa   " + Jagärzlatan + ".");
+        }
+        }
+    }
